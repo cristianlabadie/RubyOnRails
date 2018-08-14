76817d59d3ef873e3cea318d525e18972bb7907d
cristian <cristian.labadie@gmail.com>
2018-08-14 18:00:53 -0300

    variables de instancia y globales

--- holamundo.rb
+++ holamundo.rb
@@ -1,6 +1,6 @@
 =begin
 Aqui creamos la clase HolaMundo, luego de esto de ocupa el metodo initialize
-posterior creamos el metodo saluda() el cual imprime un mensaje diciendo "Hola Mundo"	
+posterior creamos el metodo saluda() el cual imprime un mensaje diciendo "Hola Mundo"
 =end
 
 =begin
@@ -9,22 +9,32 @@ to_f para convertir a float o double
 to_s para convertir a cadena simple
 
 to_str para convertir a cadena estricta
-to_int para convertir a entero estricto 
+to_int para convertir a entero estricto
 =end
 
+#probando atom para versionamiento en git
 
+#Generando variables globales
+$ejemplo = "Soy variable global"
 class HolaMundo
 	def initialize()
+		@ejemploInstancia = "soy variable de instancia"
+		
 	end
 
 	#Creando metodo para imprimir con variable
 	def saludaVariable()
 		nombre = "hola mundo"
-		puts nombre #Imprime el "Hola mundo"		
+		puts nombre #Imprime el "Hola mundo"
+		puts "------------------"
+		puts $ejemplo
+		puts "------------------"
+		puts @ejemploInstancia
 	end
 
-	#Creando metodo para imprimir 
+	#Creando metodo para imprimir
 	def saluda()
+		puts "-------------------"
 		puts "hola mundo dos"
 	end
 
@@ -32,7 +42,7 @@ class HolaMundo
 	def sumar()
 		numeroUno = 1
 		numeroDos = 3
-
+		puts "-------------------"
 		puts numeroUno + numeroDos
 	end
 
@@ -42,8 +52,9 @@ class HolaMundo
 		numeroPrimero = "1"
 		numeroSegundario = 2
 		numeroPrimero = numeroPrimero.to_i
+		puts "-------------------"
 		puts numeroPrimero + numeroSegundario
-
+		puts "-------------------"
 		numeroFloatUno = "1.2"
 		numeroFloatDos = 2.2
 		numeroFloatUno = numeroFloatUno.to_f
