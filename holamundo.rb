b1e90c511d67d8ca2eb0d7951971c845330a3973
cristian <cristian.labadie@gmail.com>
2018-08-14 16:24:43 -0300

    Adding comentaries
    Agregando tipos de comentarios a los metodos, agregando bloque de comentario al inicio del documento

--- holamundo.rb
+++ holamundo.rb
@@ -1,12 +1,14 @@
+=begin
+Aqui creamos la clase HolaMundo, luego de esto de ocupa el metodo initialize
+posterior creamos el metodo saluda() el cual imprime un mensaje diciendo "Hola Mundo"	
+=end
 class HolaMundo
 	def initialize()
 	end
 	def saluda()
-		puts "hola mundo"
-		
+		puts "hola mundo" #Imprime el "Hola mundo"		
 	end
-
+#Creando el objeto de "HolaMundo"
 	objeto = HolaMundo.new()
 	objeto.saluda
 	gets()
-	
