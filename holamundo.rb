f00e1dc963e81f11869c3d92a8da6f4b12916034
cristian <cristian.labadie@gmail.com>
2018-08-14 17:30:51 -0300

    suma de conversiones, comentarios
    breve introduccion para poder convertir valores.

--- holamundo.rb
+++ holamundo.rb
@@ -2,6 +2,17 @@
 Aqui creamos la clase HolaMundo, luego de esto de ocupa el metodo initialize
 posterior creamos el metodo saluda() el cual imprime un mensaje diciendo "Hola Mundo"	
 =end
+
+=begin
+to_i para convertir a numero
+to_f para convertir a float o double
+to_s para convertir a cadena simple
+
+to_str para convertir a cadena estricta
+to_int para convertir a entero estricto 
+=end
+
+
 class HolaMundo
 	def initialize()
 	end
@@ -24,9 +35,25 @@ class HolaMundo
 
 		puts numeroUno + numeroDos
 	end
+
+
+	#Creando metodo de suma con conversiones de tipos de datos
+	def sumarConversiones()
+		numeroPrimero = "1"
+		numeroSegundario = 2
+		numeroPrimero = numeroPrimero.to_i
+		puts numeroPrimero + numeroSegundario
+
+		numeroFloatUno = "1.2"
+		numeroFloatDos = 2.2
+		numeroFloatUno = numeroFloatUno.to_f
+		puts numeroFloatUno + numeroFloatDos
+	end
+end
 #Creando el objeto de "HolaMundo"
 	objeto = HolaMundo.new()
 	objeto.saluda
 	objeto.saludaVariable
 	objeto.sumar
+	objeto.sumarConversiones
 	gets()
