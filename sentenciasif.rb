d6feab1a8c4570008e2f87ea69501391577f3d85
cristian <cristian.labadie@gmail.com>
2018-08-17 19:39:23 -0300

    if updates

--- sentenciasif.rb
+++ sentenciasif.rb
@@ -3,11 +3,26 @@ def initialize()
 end
 def saluda()
 	hora = 10
+#IF con operador logico.	
 	if hora > 12
 		puts "buenas dias"
 	else
 		puts "buenas tardes"
 	end
+	prueba = 3
+	pruebaDos = 4
+#IF con doble condicion para que de true.
+	if prueba == 3 and pruebaDos == 4 
+		puts "correcto"
+	else
+		puts "algo salio mal"	
+	end 
+#IF con condiciones or o sino.
+	if prueba == 3 or pruebaDos == 0 or prueba < pruebaDos
+		puts "correctooooo "
+	else
+		puts "ta la caga"
+	end	
 end
 
 objeto = Sentencias.new()
